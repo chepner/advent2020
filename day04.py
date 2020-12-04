f80809078e52b09145a5dfb2032a592b58b4e034
Clint Hepner <clint.hepner@gmail.com>
2020-12-04 10:01:06 -0500

    Completed Day 4, part b
    Used "and" where I should have used "or" with the length.
    Otherwise, this contains a lot of overengineering while I
    tried to figure out which invalid passports I was allowing.

--- day04.py
+++ day04.py
@@ -14,40 +14,76 @@ class Passport:
     pid: str
     cid: str = None
 
+
+class YearError(ValueError): pass
+class YearTooEarly(YearError): pass
+class YearTooLate(YearError): pass
+
+
+def _validate_year(s, lbound, hbound):
+    if len(s) != 4:
+        raise ValueError(f"Not 4-digit year: {s}")
+    s = int(s)
+    if s < lbound:
+        raise YearTooEarly(s)
+    if s > hbound:
+        raise YearTooLate(s)
+
+
+class HeightError(ValueError): pass
+class TooShort(HeightError): pass
+class TooTall(HeightError): pass
+
+
+def _validate_height(h):
+    if h.endswith("cm"):
+        lbound = 150
+        hbound = 193
+    elif h.endswith("in"):
+        lbound = 59
+        hbound = 76
+    else:
+        raise HeightError(f"Invalid unit {h[-2:]}")
+    h = int(h[:-2])
+    if h < lbound:
+        raise TooShort(h)
+    if h > hbound:
+        raise TooTall(h)
+
+
+class PassportError(ValueError):
+    pass
+
+
 @dataclass
 class StrictPassport(Passport):
     def __post_init__(self):
-        self.byr = int(self.byr)
-        if not (1920 <= self.byr <= 2002):
-            raise ValueError(f"Invalid birth year {self.byr}")
-        self.iyr = int(self.iyr)
-        if not(2010 <= self.iyr <= 2020):
-            raise ValueError(f"Invalid issue year {self.iyr}")
-        self.eyr = int(self.eyr)
-        if not(2020 <= self.eyr <= 2030):
-            raise ValueError(f"Invalid expiration year {self.eyr}")
+        try:
+            _validate_year(self.byr, 1920, 2002)
+        except YearError as exc:
+            raise PassportError(f"Invalid birth year: {exc}")
+        try:
+            _validate_year(self.iyr, 2010, 2020)
+        except YearError as exc:
+            raise PassportError(f"Invalid issue year: {exc}")
+        try:
+            _validate_year(self.eyr, 2020, 2030)
+        except YearError as exc:
+            raise PassportError(f"Invalid expiration year: {exc}")
 
-        if self.hgt.endswith("cm"):
-            lbound = 150
-            hbound = 193
-        elif self.hgt.endswith("in"):
-            lbound = 59
-            hbound = 76
-        else:
-            raise ValueError("Invalid height unit")
+        try:
+            _validate_height(self.hgt)
+        except HeightError as exc:
+            raise PassportError(f"Invalid height: {exc}")
 
-        hgt = int(self.hgt[:-2])
-        if not(lbound <= hgt <= hbound):
-            raise ValueError("Invalid height")
-
-        if re.match("#[0-9a-fA-F]{6}", self.hcl) is None:
-            raise ValueError("Invalid hair color")
+        if re.match("#[0-9a-fA-F]{6,6}", self.hcl) is None:
+            raise PassportError(f"Invalid hair color {self.hcl}")
 
         if self.ecl not in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}:
-            raise ValueError("Invalid eye color")
+            raise PassportError(f"Invalid eye color {self.ecl}")
 
-        if len(self.pid) != 9 and not self.pid.isdigit():
-            raise ValueError("Invalid passport ID")
+        if len(self.pid) != 9 or not self.pid.isdigit():
+            raise PassportError(f"Invalid passport ID {self.pid}")
 
 
 def read_passport_data(f:Iterable[str] ) -> Iterable[dict[str,str]]:
@@ -76,7 +112,6 @@ def read_passport_data(f:Iterable[str] ) -> Iterable[dict[str,str]]:
 T = TypeVar('T', bound=Passport)
 
 def validate_passports(kind: Type[T], p: Iterable[dict[str,str]]) -> list[T]:
-    valid = []
     for i, fields in enumerate(p):
         try:
             yield kind(**fields)
@@ -84,21 +119,15 @@ def validate_passports(kind: Type[T], p: Iterable[dict[str,str]]) -> list[T]:
             print(f'Passport {i} is invalid: {exc}')
 
 
-def main_a(f: Iterable[str]):
+def _main(passport_type, f):
     passports = read_passport_data(f)
-
-    valid_passports = validate_passports(Passport, passports)
+    valid_passports = validate_passports(passport_type, passports)
     return sum(1 for v in valid_passports)
+
+
+def main_a(f: Iterable[str]):
+    return _main(Passport, f)
 
 
 def main_b(f: Iterable[str]):
-    passports = read_passport_data(f)
-
-    valid_passports = validate_passports(StrictPassport, passports)
-    return sum(1 for v in valid_passports)
-
-
-if __name__ == "__main__":
-    data = fileinput.input()
-    print(main_a(data))
-    print(main_b(data))
+    return _main(StrictPassport, f)
