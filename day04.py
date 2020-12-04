78903bd4fe7a63f51b928d25f87f6442d537ed6c
Clint Hepner <clint.hepner@gmail.com>
2020-12-04 08:06:06 -0500

    First attempt at Day 4, part a

---
+++ day04.py
@@ -0,0 +1,52 @@
+import fileinput
+from typing import Iterable
+from dataclasses import dataclass
+
+@dataclass
+class Passport:
+    byr: str
+    iyr: str
+    eyr: str
+    hgt: str
+    hcl: str
+    ecl: str
+    pid: str
+    cid: str
+
+
+def read_passport_data(f:Iterable[str] ) -> Iterable[dict[str,str]]:
+    current = []
+    for line in f:
+        line = line.strip()
+        if line:
+            fields = line.split()
+            current.extend(fields)
+        elif current:
+            yield dict(field.split(":") for field in current)
+            current = []
+
+
+def validate_passports(p: Iterable[dict[str,str]]) -> list[Passport]:
+    valid = []
+    for fields in p:
+        try:
+            yield Passport(**fields)
+        except TypeError:
+            pass
+
+
+def main_a(f: Iterable[str]):
+    passports = read_passport_data(f)
+
+    valid_passports = validate_passports(passports)
+    return sum(1 for v in valid_passports)
+
+
+def main_b(map_data):
+    pass
+
+
+if __name__ == "__main__":
+    data = fileinput.input()
+    print(main_a(data))
+    print(main_a(data))
