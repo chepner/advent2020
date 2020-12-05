748129d045e174c9d5aa0b88a6902318e0efe33a
Clint Hepner <clint.hepner@gmail.com>
2020-12-05 16:18:02 -0500

    Refactor to prepare for part b

--- day05.hs
+++ day05.hs
@@ -2,8 +2,10 @@ import Data.Bifunctor
 import Data.Functor
 import Control.Arrow
 import Control.Monad
+import Data.List
 
 data HiLo = Hi | Lo deriving Show
+type Seat = (Int, Int)
 
 bsp ::  Int -> Int -> [HiLo] -> Int
 bsp low high xs  = go xs low high
@@ -25,13 +27,22 @@ seat :: Char -> HiLo
 seat 'L' = Lo
 seat 'R' = Hi
 
-passToSeatID :: ([HiLo], [HiLo]) -> Int
-passToSeatID (row, seat) = 8*r + c
+passToSeat :: ([HiLo], [HiLo]) -> Seat
+passToSeat (row, seat) = (r, c)
    where r = bsp 0 127 row
          c = bsp 0 7 seat
 
+seatToSeatID :: Seat -> Int
+seatToSeatID (r, c) = 8*r + c
+
+passToSeatID :: ([HiLo], [HiLo]) -> Int
+passToSeatID = seatToSeatID . passToSeat
+
+allSeats :: [Seat]
+allSeats = [(r, c) | r <- [0..127], c <- [0..7]]
 
 main = do
   contents <- readFile "day05.input"
-  print $ maximum $ map (passToSeatID . parseBoardingPass) (lines contents)
-
+  let boardingPasses =  map parseBoardingPass (lines contents)
+      seats = sort $ map passToSeat boardingPasses
+  print $ maximum (map seatToSeatID seats)
