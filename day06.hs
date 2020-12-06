b579530e780cf3347d67163c3bb79ac87260b4c6
Clint Hepner <clint.hepner@gmail.com>
2020-12-06 09:42:33 -0500

    Finished Day 6, part b

--- day06.hs
+++ day06.hs
@@ -1,5 +1,8 @@
 import System.Environment
-import Data.List (nub) -- I know, it's quadratic...
+import Data.List ( nub -- I know, it's quadratic...
+                 , intersect
+                 )
+                  
 
 -- Assumes well-behaved input
 groupSplit :: [String] -> [[String]]
@@ -12,6 +15,9 @@ main = do
   fname <- fmap (!! 0) getArgs
   contents <- readFile fname
   let groups  = groupSplit (lines contents)
+
+  putStrLn "Part a"
   print $ sum $ map (length . nub. concat) groups
-  
-  
+
+  putStrLn "Part b"
+  print $ sum $ map (length . (foldr1 intersect)) groups
