f62fe303b18724aec9d3be4743a27adac72ed069
Clint Hepner <clint.hepner@gmail.com>
2020-12-07 15:04:56 -0500

    Finished Day 7, part b

--- day07.hs
+++ day07.hs
@@ -38,27 +38,47 @@ nocontents :: ReadP [(Color, Int)]
 nocontents = string "no other bags" >> return []
 
 -- Maps a color with the colors that can directly contain it
-type Graph = M.Map Color [Color]
+type GraphA = M.Map Color [Color]
+type GraphB = M.Map Color [(Color, Int)]
 
-rule :: ReadP Graph
+rule :: ReadP (Color, [(Color, Int)])
 rule = do outer <- color
           string " bags contain"
           skipSpaces
           list <- choice [nocontents, sepBy1 contents (string ", ")]
           string "."
-          return $ M.fromList [ (fst x, [outer]) | x <- list]
+          return (outer, list)
+          -- return $ M.fromList [ (fst x, [outer]) | x <- list]
 
 
 -- Graph search to find all colors that can transitively contain a given color
-getContainers :: Graph -> Color -> [Color]
+getContainers :: GraphA -> Color -> [Color]
 getContainers g c = let direct = fromMaybe [] (M.lookup c g)
                     in direct ++ concatMap (getContainers g) direct
+
+
+getContained :: GraphB -> Color -> Int
+getContained g c = let contents = fromMaybe [] (M.lookup c g)
+                       colors = map fst contents
+                       counts = map snd contents
+                       contained = sum ( zipWith (*) (map (getContained g) colors) counts)
+                   in sum counts + contained
+
 
 main = do
    args <- getArgs
    let fname = args !! 0
    text_rules <- lines <$> readFile fname
 
-   let graph = M.unionsWith (++) $ map (fst . head . readP_to_S rule) text_rules
+   let rules = map (fst . head . readP_to_S rule) text_rules
 
-   print $ length $ nub $ getContainers graph "shiny gold"
+   let alistPartA = map (\(c, cs) -> [(x, [c]) | (x,_) <- cs]) rules
+   let graphPartA = M.unionsWith (++) $ map M.fromList alistPartA
+
+   print "Part a"
+   print $ length $ nub $ getContainers graphPartA "shiny gold"
+
+   print "Part b"
+   let graphPartB = M.fromList rules
+   print $ getContained graphPartB "shiny gold"
+
