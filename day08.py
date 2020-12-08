06cfefecafbc4eec2f82330b9547363266fbf53b
Clint Hepner <clint.hepner@gmail.com>
2020-12-08 06:49:56 -0500

    Refactor and prep for part b
    We need to fix the program so that it termiantes.
    Currently, I "fix" the program by just returning it as-is
    and letting it fail.

--- day08.py
+++ day08.py
@@ -4,39 +4,94 @@ from dataclasses import dataclass
 import re
 
 
+class ProgramError(RuntimeError):
+    def __init__(self, *, ps, **kwargs):
+        super().__init__(**kwargs)
+        self.program_state = ps
+
+
+class InfiniteRecursionError(ProgramError):
+    pass
+
+
+class InvalidOpcodeError(ProgramError):
+    def __init__(self, *, opcode, **kwargs):
+        super().__init__(**kwargs)
+        self.opcode = opcode
+
+
+class ProgramState:
+    def __init__(self, *, pc, acc):
+        self.pc = pc
+        self.acc = acc
+
+
 def run_program(prg):
-    pc = 0
-    acc = 0
+    state = ProgramState(pc=0, acc=0)
 
     seen = set()
     while True:
-        instr, arg = prg[pc]
+        try:
+            instr, arg = prg[state.pc]
+        except IndexError:
+            return state
 
-        if pc in seen:
-            print(f"Infinite loop detected, accumulator = {acc}")
-            break
+        if state.pc in seen:
+            raise InfiniteRecursionError(ps=state)
 
-        seen.add(pc)
+        seen.add(state.pc)
+
         if instr == "nop":
-            pc += 1
+            state.pc += 1
         elif instr == "acc":
-            acc += arg
-            pc += 1
+            state.acc += arg
+            state.pc += 1
         elif instr == "jmp":
-            pc += arg
+            state.pc += arg
         else:
-            raise RuntimeError(f"Error: unrecognized opcode {instr}")
+            raise InvalidOpcodeError(ps=state, opcode=instr)
 
 
-def main_a(f: Iterable[str]):
+def alt_programs(prg):
+    yield prg
+
+            
+# Change a jmp to a no-op or vice versa
+def fix_program(prg):
+
+    for p in alt_programs(prg):
+        try:
+            rv = run_program(p)
+            print(f"Accumulator = {rv}")
+            return p
+        except ProgramError:
+            pass
+
+    raise RuntimeError("Cannot fix infinite recursion") 
+
+
+def parse_program(f):
     program = []
     for line in f:
         instruction, arg = line.strip().split()
         arg = int(arg)
         program.append((instruction, arg))
+    return program
 
-    run_program(program)
+
+def main_a(f: Iterable[str]):
+    program = parse_program(f)
+    try:
+        state = run_program(program)
+    except InfiniteRecursionError as exc:
+        return exc.program_state.acc
 
 
 def main_b(f: Iterable[str]):
-    pass
+    program = parse_program(f)
+    try:
+        program = fix_program(program)
+        state = run_program(program)  # Yes, we already ran it.
+        return state.acc
+    except RuntimeError:
+        print("Unable to fix program")
