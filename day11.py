38883848b727e41d3f4c22fbeaaa3ab79574fec8
Clint Hepner <clint.hepner@gmail.com>
2020-12-11 07:00:28 -0500

    Refactor for part b
    I still need to redefine neighbors_b to take
    visibility into account.

--- day11.py
+++ day11.py
@@ -16,7 +16,7 @@ def get(floorplan, s):
         return floorplan[y][x]
 
 
-def neighbors(floorplan, s):
+def neighbors_a(floorplan, s):
     x,y = s
     for xd, yd in product(range(-1, 2), repeat=2):
         if xd == yd == 0:
@@ -25,20 +25,29 @@ def neighbors(floorplan, s):
             yield n
 
 
-def transition(floorplan, s):
+def neighbors_b(floorplan, s):
+    x,y = s
+    for xd, yd in product(range(-1, 2), repeat=2):
+        if xd == yd == 0:
+            continue
+        if (n := get(floorplan, (x+xd, y+yd))) is not None:
+            yield n
+
+
+def transition(floorplan, s, neighbors, c):
     state = get(floorplan, s)
     if state == 'L' and all(x != '#' for x in neighbors(floorplan, s)):
         return '#'
-    elif state == '#' and sum(x == '#' for x in neighbors(floorplan, s)) >= 4:
+    elif state == '#' and sum(x == '#' for x in neighbors(floorplan, s)) >= c:
         return 'L'
     else:
         return state
 
 
-def step(floorplan):
+def step(floorplan, neighbors, c):
     width = len(floorplan[0])
     height = len(floorplan)
-    return [[transition(floorplan, (x,y)) for x in range(width)] for y in range(height)]
+    return [[transition(floorplan, (x,y), neighbors, c) for x in range(width)] for y in range(height)]
 
 
 def print_floorplan(floorplan):
@@ -50,11 +59,11 @@ def count_occupied(floorplan):
     return sum(sum(x == '#' for x in row) for row in floorplan)
 
 
-def main_a(f: Iterable[str]):
+def _main(f:Iterable[str], neighbors, c):
     floorplan = parse_floorplan(f)
     count = 0
     while True:
-        next_floorplan = step(floorplan)
+        next_floorplan = step(floorplan, neighbors, c)
         if next_floorplan == floorplan:
             break
         count += 1
@@ -62,5 +71,9 @@ def main_a(f: Iterable[str]):
     return count_occupied(floorplan)
 
 
+def main_a(f: Iterable[str]):
+    return _main(f, neighbors_a, 4)
+
+
 def main_b(f: Iterable[str]):
-    floorplan = parse_floorplan(f)
+    return _main(f, neighbors_b, 5)
