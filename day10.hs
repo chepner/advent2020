37f43c44fdc4f8b333e8154c67bbe36315d9bc54
Clint Hepner <clint.hepner@gmail.com>
2020-12-10 21:43:39 -0500

    Day 10, part b in progress
    Theoretically, I think this works. It works on the 31-line sample,
    anyway. Unfortunately, it's far too slow to compute the answer
    on my 100+-line input, due to the Fibonacci-like recursion.
    
    Time to figure out how to memoize this.

--- day10.hs
+++ day10.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 import qualified Options.Applicative as OA
-import Data.List (sort)
+import Data.List (sort, tails)
 import Data.Function ((&))
+import Control.Monad (guard)
 
-newtype Options = Options {
-  fileName :: FilePath
+data Options = Options {
+    fileName :: FilePath
+  , count :: Int
 }
 
 options :: OA.Parser Options
-options = Options <$> OA.argument OA.str (OA.metavar "FILE")
+options = Options
+          <$> OA.argument OA.str (OA.metavar "FILE")
+          <*> OA.option OA.auto (OA.long "count" <> OA.value 20)
 
 p = OA.info (options OA.<**> OA.helper)
     ( OA.fullDesc
-      <> OA.progDesc "Solve Day 9 for Advent of Code 2020"
-      <> OA.header "Advent 2020: Day 9" )
+      <> OA.progDesc "Solve Day 10 for Advent of Code 2020"
+      <> OA.header "Advent 2020: Day 10" )
 
 
 readNumbers :: FilePath -> IO [Int]
@@ -30,9 +34,28 @@ diffs xs = (zipWith (-) (tail xs) xs )
 part_a :: [Int] -> Int
 part_a xs = length (filter (==3) xs) * length (filter (== 1) xs)
 
+
+arrangements :: [Int] -> [[Int]]
+arrangements [x] = [[x]]
+arrangements (x:xs) = let p [] = False
+                          p (y:_) = y <= x + 3
+                          continuations = filter p (tails xs)
+                      in map (x:) (continuations >>= arrangements)
+
+countArrangements :: [Int] -> Int
+countArrangements [x] = 1
+countArrangements (x:xs) = let p [] = False
+                               p (y:_) = y <= x + 3
+                               continuations = filter p (tails xs)
+                           in sum (map countArrangements continuations)
+
 main = do
    args <- OA.execParser p
    numbers <- readNumbers (fileName args)
 
    putStrLn "Part a"
    makeList numbers & diffs & part_a & print
+
+   putStrLn "Part b"
+   print (makeList numbers)
+   makeList numbers & take (count args) & countArrangements & print
