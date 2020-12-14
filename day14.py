ddc0f556262e1f4fa919f6a6891d428b5b7bdd67
Clint Hepner <clint.hepner@gmail.com>
2020-12-14 11:47:09 -0500

    Finished Day 14, part b
    Two big hangups:
      1. I may have misinterpreted how the memory address decoder worked.
         I ended up rewriting it, but...
      2. I missed that the V2 decoder didn't touch the value. Once I rewrote
         the address decoder, and wrote the value as read, it worked.

--- day14.py
+++ day14.py
@@ -1,6 +1,8 @@
 import fileinput
 from typing import Iterable, Type, TypeVar
+from itertools import chain, combinations
 import re
+
 
 def parse_program(f):
     program = []
@@ -8,7 +10,10 @@ def parse_program(f):
         line = line.strip()
         if line.startswith("mask"):
             mask = line.split()[2]
-            mask = ("mask", int(mask.replace("X", "1"), base=2), int(mask.replace("X", "0"), base=2))
+            mask = ("mask",
+                    int(mask.replace("X", "1"), base=2),
+                    int(mask.replace("X", "0"), base=2),
+                    mask)
             program.append(mask)
         else:
             m = re.match(r"mem\[(.*)\] = (.*)", line)
@@ -36,15 +41,48 @@ def run_program(p):
     return mem
             
 
-def _main(f:Iterable[str]):
+def power(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+
+def addresses(mask):
+    if "X" not in mask:
+        mask = int(mask, base=2)
+        yield mask
+    else:
+        yield from addresses(mask.replace("X", "1", 1))
+        yield from addresses(mask.replace("X", "0", 1))
+
+
+def v2_mask(addr, mask):
+    return "".join( "X" if m == "X" else "1" if m == "1" else a for a, m in zip(addr, mask))
+
+
+def run_program_v2(p):
+    mem = {}
+    for instr in p:
+        if instr[0] == "mask":
+            mask = instr[1:]
+        elif instr[0] == "assign":
+            addr, val = instr[1:]
+            addr_bin = f'{bin(addr)[2:]:0>36}'
+            addr_template = v2_mask(addr_bin, mask[2])
+            for real_addr in addresses(addr_template):
+                mem[real_addr] = val
+
+    return mem
+
+
+def _main(f:Iterable[str], d):
     program = parse_program(f)
-    memory = run_program(program)
+    memory = d(program)
     return sum(memory.values())
 
 
 def main_a(f: Iterable[str]):
-    return _main(f)
+    return _main(f, run_program)
 
 
 def main_b(f: Iterable[str]):
-    return _main(f)
+    return _main(f, run_program_v2)
