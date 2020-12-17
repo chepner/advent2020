36c8ff0dd6685a8f0427aec17cc3e89e6d1aa1c1
Clint Hepner <clint.hepner@gmail.com>
2020-12-17 15:06:03 -0500

    Install ghcid

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@
   pkgs.mkShell {
     nativeBuildInputs = [
          pkgs.buildPackages.python39
+         pkgs.buildPackages.ghcid
          (pkgs.buildPackages.haskellPackages.ghcWithPackages (p: [
              p.optparse-applicative
              p.memoize
